b4b090777d738ad4c8836d6be480a564020c4c9e
yasirulakshitha <gamagejanithyasirulakshitha@gmail.com>
2025-06-21 09:14:57 +0530

    update the CORS origin URL to the deployed frontend URL

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({origin: "https://fee-storefront-backend-hylf.onrender.com"}));
 
 app.use("/api/products", productsRouter);
 app.use("/api/categories", categoriesRouter);
